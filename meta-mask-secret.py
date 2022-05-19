1179e8ff9e5cce938ad44b86ce69442e5ccccdca
maryam <maryam.poortarigh@gmail.com>
2022-05-19 20:21:48 +0430

    Metamask secrate phrase and other commands

--- meta-mask-secret.py
+++ meta-mask-secret.py
@@ -2,10 +2,19 @@
 
 Metamask secret recovery phrase:
 
-
 brother key audit vicious fold expect baby truck civil path sauce horror
 
-
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 git token:
-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
+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
+
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+Ganache installation:
+
+chmod +x ganache-2.5.4-linux-x86_64.AppImage
+
+./ganache-2.5.4-linux-x86_64.AppImage
+
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
